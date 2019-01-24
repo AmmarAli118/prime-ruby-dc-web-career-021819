f37327cc9d55130d23684859096283a614bdfc42
Ammar Ali <funnyguy118@gmail.com>
2019-01-24 18:04:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,4 @@
 # Add  code here!
 def prime? (int)
-  for i in 2..int-1
-    if int % i == 0 
-      false
-    else
-      false
-    end
-  end
-end
 
-#if int % 2 == 0 || int % 3 == 0 || int % 5 == 0
-#    true 
-#  else
-#    false
-#  end
+end
