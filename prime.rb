eec70ebe6214a9f6e47a0a1672bff99186eca3bd
Ammar Ali <funnyguy118@gmail.com>
2019-01-24 18:00:47 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
 # Add  code here!
 def prime? (int)
-  if int % 2 == 0 || int % 3 == 0 || int % 5 == 0
-    true 
-  else
-    false
-  end 
+  for i in 2--int-1
 end
+
+#if int % 2 == 0 || int % 3 == 0 || int % 5 == 0
+#    true 
+#  else
+#    false
+#  end
